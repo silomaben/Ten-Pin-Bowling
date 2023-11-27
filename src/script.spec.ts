b7afe40f7b7e9d57787be8127ba1b407ee57697b
Benard Siloma Masikonde <benard.masikonde@thejitu.com>
2023-11-27 10:54:40 +0300

    to fix test setup

--- src/script.spec.ts
+++ src/script.spec.ts
@@ -1,11 +1,21 @@
 import { TenPinGame } from "./script";
 
-describe('Note controllers',()=>{
+describe('check correct result scores',()=>{
 
-    it('it should verify if no token it throws an error', async ()=>{
+    it('should pass if all rolls are strikes returns a score of 300', async ()=>{
+
+        const game = new TenPinGame('X X X X X X X X X X X X ');
+      expect(game.getScore()).toBe(300);
+    })
+    it('should pass if score score is 90 for 9- 9- 9- 9- 9- 9- 9- 9- 9- 9-', async ()=>{
 
         const game = new TenPinGame('X X X X X X X X X X X X ');
       expect(game.getScore()).toBe(90);
+    })
+    it('should pass if score score is 150 for 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5', async ()=>{
+
+        const game = new TenPinGame('X X X X X X X X X X X X ');
+      expect(game.getScore()).toBe(150);
     })
 
 
